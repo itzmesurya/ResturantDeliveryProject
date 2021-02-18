448bcf3dee9df54246497649db51bd7b7f3c50b4
Yong Wang <frawang211@gmail.com>
2021-02-18 10:05:32 -0500

    updated client file

--- src/Client.java
+++ src/Client.java
@@ -8,13 +8,3 @@ public class Client extends User {
 	}
 	
 }
-
-
-public class Client extends User {
-	
-	public Client(String username, String password, String first_name, String last_name, String address, String email, String phone) {
-		super(username, password, first_name, last_name, address, email, phone);
-		this.level = 0;
-	}
-	
-}
