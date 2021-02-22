7cc847de78b57d6a12d6e0e096b4d1964141fa27
Alex McKenzie <alex-mckenzie@live.com>
2021-02-22 08:43:30 -0500

    work on models and database

--- src/Manager.java
+++ src/Manager.java
@@ -1,8 +1,30 @@
 
+/**
+ * @author Alex
+ * Manager can create, edit and delete restaurants. Can only edit and delete restaurants they created.
+ */
 public class Manager extends User {
 	
+	/**
+	 * @param username
+	 * @param password
+	 * @param first_name
+	 * @param last_name
+	 * @param address
+	 * @param email
+	 * @param phone
+	 */
 	public Manager(String username, String password, String first_name, String last_name, String address, String email, String phone) {
 		super(username, password, first_name, last_name, address, email, phone);
+		this.level = 3;
+	}
+	
+	/**
+	 * @param username
+	 * @param password
+	 */
+	public Manager(String username, String password) {
+		super(username, password);
 		this.level = 3;
 	}
 	
