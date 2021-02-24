4ca094c476089e46b76b973f4f0d444b2ade9c59
Alex McKenzie <alex-mckenzie@live.com>
2021-02-24 10:26:47 -0500

    working on main

--- com/restaurantdeliverycontrollers/LoginCore.java
+++ com/restaurantdeliverycontrollers/LoginCore.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 import com.restaurantdeliverymodels.Admin;
 import com.restaurantdeliverymodels.Database;
+import com.restaurantdeliveryviews.Admin_main_menu_Panel;
 import com.restaurantdeliveryviews.LoginPanel;
 import com.restaurantdeliveryviews.MainFrame;
 
@@ -14,14 +15,11 @@ public class LoginCore {
 	public LoginCore() {
 		LoginPanel.getLoginBtn().addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				MainFrame.changePanel(new Admin_main_menu_Panel());
 				ArrayList<Admin> admins = Database.getAdmins();
 				for (Admin admin : admins) {
 					if (admin.getUsername().equals(LoginPanel.getUsernameTF().getText()) && admin.getPassword().equals(LoginPanel.getPasswordTF().getText())) {
 						Main.user = Database.getAdminById(admin.getId());
-						
-						switch(Main.user.getLevel()) {
-							
-						}
 					}
 				}
 				
