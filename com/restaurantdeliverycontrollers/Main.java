44548c886fba417c51ada8aca6f94c514c0c1045
Vairag <Vairag4u@gmail.com>
2021-02-24 10:26:01 -0400

    Delivery files

--- com/restaurantdeliverycontrollers/Main.java
+++ com/restaurantdeliverycontrollers/Main.java
@@ -11,6 +11,10 @@ import com.restaurantdeliverymodels.Order;
 import com.restaurantdeliverymodels.Restaurant;
 import com.restaurantdeliverymodels.Restaurateur;
 import com.restaurantdeliverymodels.User;
+import com.restaurantdeliveryviews.Accept_Done__Delivery_Panel;
+import com.restaurantdeliveryviews.Admin_main_menu_Panel;
+import com.restaurantdeliveryviews.Client_main_menu_Panel;
+import com.restaurantdeliveryviews.DeliveryPanel;
 import com.restaurantdeliveryviews.LoginPanel;
 import com.restaurantdeliveryviews.MainFrame;
 
@@ -21,7 +25,7 @@ public class Main {
 	public static void main(String[] args) {
 		new Database();
 		new MainFrame();
-		MainFrame.changePanel(new LoginPanel());
+		MainFrame.changePanel(new DeliveryPanel("create"));
 		
 	}
 
