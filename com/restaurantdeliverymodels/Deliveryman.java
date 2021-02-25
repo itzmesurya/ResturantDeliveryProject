8361cb5788bc73ba09c619d2b4e3e11532197ad3
Vairag <Vairag4u@gmail.com>
2021-02-25 10:06:39 -0400

    Delivery core and panel

--- com/restaurantdeliverymodels/Deliveryman.java
+++ com/restaurantdeliverymodels/Deliveryman.java
@@ -19,6 +19,7 @@ public class Deliveryman extends User {
 	 * @param phone
 	 * @param delivery_areas
 	 */
+	
 	public Deliveryman(String username, String password, String first_name, String last_name, String address, String email, String phone, String[] delivery_areas) {
 		super(username, password, first_name, last_name, address, email, phone);
 		this.level = 1;
