d817b906bcbceb9822982ab73933e0d00ec078dc
Aruna K N <aruna.k_n@contractors.roche.com>
2021-02-25 20:46:11 +0530

    Account core changes

--- com/restaurantdeliverycontrollers/MenuCore.java
+++ com/restaurantdeliverycontrollers/MenuCore.java
@@ -1,5 +1,46 @@
 package com.restaurantdeliverycontrollers;
 
-public class MenuCore {
+import javax.swing.table.DefaultTableModel;
 
+import com.restaurantdeliverymodels.Database;
+import com.restaurantdeliverymodels.Item;
+import com.restaurantdeliveryviews.AccountPanel;
+import com.restaurantdeliveryviews.MenuPanel;
+
+public class MenuCore {
+	MenuPanel menuPanel;
+
+	MenuCore() {
+		
+		BindActionEvents();
+	}
+
+	private void BindActionEvents() {
+		MenuPanel.getAddBtn().addActionListener(e -> addItemToMenuTable());
+		MenuPanel.getSaveBtn().addActionListener(e -> {
+
+		});
+		MenuPanel.getBtnEdit().addActionListener(e -> {
+
+		});
+		MenuPanel.getBtnDelete().addActionListener(e -> {
+
+		});
+		MenuPanel.getSelectBtn().addActionListener(e -> {
+
+		});
+
+	}
+
+	public void addItemToMenuTable() {
+		Database.addItem(new Item("", 0));
+		DefaultTableModel model = (DefaultTableModel) menuPanel.getTable().getModel();
+		model.addRow(new Object[] { MenuPanel.getItemNameTextField(), MenuPanel.getItemPriceTextField() });
+	}
+
+	public void saveItemToDatabase() {
+		String itemObtainedFromUI = MenuPanel.getItemNameTextField().getText();
+		String itempriceObtainedFromUi = MenuPanel.getItemPriceTextField().getText();
+		Database.getItems();
+	}
 }
