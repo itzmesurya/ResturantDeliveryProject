2a3c7880c00988fa0384d7022c9049830cf308d8
Vairag <Vairag4u@gmail.com>
2021-03-03 11:42:41 -0400

    Order core

--- com/restaurantdeliverycontrollers/OrderCore.java
+++ com/restaurantdeliverycontrollers/OrderCore.java
@@ -1,27 +1,194 @@
 package com.restaurantdeliverycontrollers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+
 import com.restaurantdeliverymodels.CRUDAction;
+import com.restaurantdeliverymodels.Client;
+import com.restaurantdeliverymodels.Database;
+import com.restaurantdeliverymodels.Item;
 import com.restaurantdeliverymodels.Order;
+import com.restaurantdeliverymodels.Restaurant;
 import com.restaurantdeliverymodels.Restaurateur;
 import com.restaurantdeliveryviews.DeliveryPanel;
 import com.restaurantdeliveryviews.OrderPanel;
 
 public class OrderCore {
+	OrderPanel panel;
+	ArrayList<Order> orders = new ArrayList<Order>();
+	Order order;
 
-	 OrderCore(CRUDAction action) {
+ 	OrderCore(OrderPanel orderPanel, CRUDAction action) {
+ 		this.panel = orderPanel;
 		
 		 switch(action) {
 		 case Read:
+			 panel.getAccept_btn().setVisible(false);
+			 ArrayList<Restaurant> restaurants = Database.getRestaurants();
+			 for(Restaurant restaurant: restaurants) {
+				 panel.getSelectR_comboBox().addItem(restaurant.getName());
+			 }
+			 panel.getSelectR_comboBox().addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					changeFilters();
+				}
+			});
+			panel.getProgress_RadioButton().addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					changeFilters();
+				}
+			});
+			panel.getIncoming_RadioButton().addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					changeFilters();
+				}
+			});
 			 break;
+			 
 		 case Accept:
+			 panel.getIncoming_RadioButton().setVisible(false);
+			 panel.getProgress_RadioButton().setVisible(false);
+			 panel.getSelectR_comboBox().setVisible(false);
+			 
+			 orders = ((Restaurateur) Main.user).getOrders();
+//			 orders = ((Restaurateur) Main.user).getOrdersToAccept();
+			 
+			 displayOrders();
+			 
+			 panel.getAccept_btn().addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if(order != null)
+						order.markDeliveryAccepted();
+
+					 orders = ((Restaurateur) Main.user).getOrdersToAccept();
+					 displayOrders();
+				}
+			});
 			 break;
+			 
 		 case Ready:
+			 panel.getIncoming_RadioButton().setVisible(false);
+			 panel.getProgress_RadioButton().setVisible(false);
+			 panel.getSelectR_comboBox().setVisible(false);
+
+			 orders = ((Restaurateur) Main.user).getOrders();
+//			 orders = ((Restaurateur) Main.user).getOrdersToMarkReady();
+			 
+			 displayOrders();
+
+			 panel.getAccept_btn().setText("Ready");
+			 
+			 panel.getAccept_btn().addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if(order != null)
+						order.markReadyForDelivery();
+					
+					 orders = ((Restaurateur) Main.user).getOrdersToMarkReady();
+					 displayOrders();
+				}
+			});
 			 break;
 		 }
-		 
-		ArrayList<Order> orders = ((Restaurateur) Main.user).getOrders();
-	}
+ 	}
+ 	
+ 	private void changeFilters() {
+		String restName = (String) panel.getSelectR_comboBox().getSelectedItem();
+		Restaurant restaurant = Database.getRestaurantByName(restName);
+		ArrayList<Order> allOrders = restaurant.getOrders();
+		int status = 0;
+		for (Order order: allOrders) {
+//			if (order.getStatus() == status)
+				orders.add(order);
+		}
+		displayOrders();
+ 	}
+ 	
+ 	private void displayOrders() {
+ 		panel.getTextField().setText(null);
+ 		panel.getTextField_1().setText(null);
+ 		panel.getTextField_2().setText(null);
+ 		panel.getTextField_3().setText(null);
+// 		DefaultTableModel oldModel = (DefaultTableModel) panel.getTable().getModel();
+// 		oldModel.setRowCount(0);
+ 		DefaultTableModel model = new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Meal in Order",
+				"Quantity"
+			}
+		);
+ 		panel.getTable().setModel(model);
+ 	
+
+ 		DefaultTableModel oldModel_1 = (DefaultTableModel) panel.getTable_1().getModel();
+ 		oldModel_1.setRowCount(0);
+
+ 		
+		DefaultTableModel model_1 = new DefaultTableModel(
+				new Object[][] {
+				},
+				new String[] {
+					"Order Id",
+					"Client Name" 
+				}
+			);
+		for(Order order: orders) {
+			Client client = Database.getClientById(order.getClient_id());
+			if(client != null) {
+		 		Object[] row = { order.getId(), client.getFirst_name() + " " + client.getLast_name() };
+				model_1.addRow(row);
+			}
+		}
+ 		panel.getTable_1().setModel(model_1);
+ 		panel.getTable_1().getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+			
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				int selRow = panel.getTable_1().getSelectedRow();
+				if(selRow == -1)
+					return;
+ 	            int orderid = (int) panel.getTable_1().getValueAt(selRow, 0);
+ 	            order = Database.getOrderById(orderid);
+ 	            Item[] items = order.getItems();
+// 	    		DefaultTableModel oldModel = (DefaultTableModel) panel.getTable().getModel();
+// 	    		oldModel.setRowCount(0);
+ 	   		
+	 	   		DefaultTableModel model = new DefaultTableModel(
+	 	   				new Object[][] {
+	 	   				},
+	 	   				new String[] {
+	 	   					"Meal in Order",
+	 	   					"Quantity"
+	 	   				}
+	 	   			);
+	 	   		for(Item item: items) {
+	 	   	 		Object[] row = { item.getName(), item.getQuantity() };
+	 	   			model.addRow(row);
+	 	   		}
+	 	    	panel.getTable().setModel(model);
+	 	    	
+	 	 		panel.getTextField().setText(order.getDate());
+	 	 		panel.getTextField_1().setText(order.getHour());
+	 	 		panel.getTextField_2().setText(order.getMinute());
+	 	 		panel.getTextField_3().setText(order.getDelivery_address());
+ 	        }
+ 	    });
+ 	}
 
 }
--- com/restaurantdeliveryviews/DeliveryView.java
+++ com/restaurantdeliveryviews/DeliveryView.java
@@ -60,11 +60,13 @@ public class DeliveryView {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.WHITE);
 		frame.setBackground(Color.YELLOW);
 		frame.setBounds(100, 100, 1200, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -81,6 +83,7 @@ public class DeliveryView {
 		);
 		
 		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.WHITE);
 		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel_1.setForeground(Color.BLACK);
 		
@@ -88,11 +91,11 @@ public class DeliveryView {
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		
 		Name_Field = new JTextField();
-		Name_Field.setBackground(new Color(127, 255, 212));
+		Name_Field.setBackground(Color.white);
 		Name_Field.setColumns(10);
 		
 		JLabel lblPostalCode = new JLabel("TelePhone Number");
-		lblPostalCode.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblPostalCode.setFont(new Font("Arial", Font.PLAIN, 14));
 		
 		Phone_Field = new JTextField();
 		Phone_Field.setBackground(new Color(127, 255, 212));
@@ -104,18 +107,19 @@ public class DeliveryView {
 		gl_panel_1.setHorizontalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_1.createSequentialGroup()
-					.addContainerGap()
+					.addGap(47)
 					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblPersnolInformation)
-						.addGroup(gl_panel_1.createSequentialGroup()
-							.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblNewLabel_1)
-								.addComponent(lblPostalCode))
-							.addGap(43)
-							.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING)
-								.addComponent(Phone_Field, GroupLayout.PREFERRED_SIZE, 162, GroupLayout.PREFERRED_SIZE)
-								.addComponent(Name_Field, GroupLayout.PREFERRED_SIZE, 162, GroupLayout.PREFERRED_SIZE))))
-					.addContainerGap(60, Short.MAX_VALUE))
+						.addComponent(lblNewLabel_1)
+						.addComponent(lblPostalCode))
+					.addGap(114)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING)
+						.addComponent(Phone_Field, GroupLayout.DEFAULT_SIZE, 350, Short.MAX_VALUE)
+						.addComponent(Name_Field, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 350, Short.MAX_VALUE))
+					.addGap(35))
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblPersnolInformation, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(461, Short.MAX_VALUE))
 		);
 		gl_panel_1.setVerticalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
@@ -124,22 +128,23 @@ public class DeliveryView {
 					.addComponent(lblPersnolInformation, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
 					.addGap(21)
 					.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_1)
-						.addComponent(Name_Field, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(Name_Field, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_1))
 					.addGap(18)
 					.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblPostalCode, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE)
-						.addComponent(Phone_Field, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(30, Short.MAX_VALUE))
+						.addComponent(Phone_Field, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblPostalCode, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(21, Short.MAX_VALUE))
 		);
 		panel_1.setLayout(gl_panel_1);
 		
 		JButton Uavailable_btn = new JButton("Veryfy Username availability");
-		Uavailable_btn.setBackground(Color.YELLOW);
-		Uavailable_btn.setForeground(Color.BLACK);
-		Uavailable_btn.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		Uavailable_btn.setBackground(Color.BLACK);
+		Uavailable_btn.setForeground(Color.white);
+		Uavailable_btn.setFont(new Font("Arial", Font.BOLD, 14));
 		
 		JLabel lblNewLabel_2 = new JLabel("Select :");
+		lblNewLabel_2.setBackground(Color.RED);
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		
 		JComboBox selectR_comboBox = new JComboBox();
@@ -150,71 +155,75 @@ public class DeliveryView {
 		panel_3.setBorder(new LineBorder(new Color(0, 0, 0)));
 		
 		JPanel panel_4 = new JPanel();
+		panel_4.setBackground(Color.WHITE);
 		panel_4.setBorder(new LineBorder(new Color(0, 0, 0)));
 		
 		JPanel panel_5 = new JPanel();
+		panel_5.setBackground(Color.WHITE);
 		panel_5.setBorder(new LineBorder(new Color(0, 0, 0)));
 		
 		JButton Save_btn = new JButton("Save");
-		Save_btn.setForeground(Color.BLACK);
-		Save_btn.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		Save_btn.setBackground(Color.YELLOW);
+		Save_btn.setForeground(Color.white);
+		Save_btn.setFont(new Font("Arial", Font.BOLD, 14));
+		Save_btn.setBackground(Color.BLACK);
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.TRAILING)
+			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
 							.addGroup(gl_panel.createSequentialGroup()
 								.addGap(50)
-								.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE)
-								.addPreferredGap(ComponentPlacement.RELATED, 332, Short.MAX_VALUE)
+								.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED, 426, Short.MAX_VALUE)
 								.addComponent(lblNewLabel_2)
 								.addGap(27))
 							.addGroup(gl_panel.createSequentialGroup()
-								.addContainerGap(199, Short.MAX_VALUE)
-								.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING, false)
-									.addComponent(panel_4, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-									.addComponent(panel_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-								.addGap(124)))
-						.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
-							.addContainerGap()
-							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+								.addGap(62)
+								.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+									.addComponent(panel_4, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 664, Short.MAX_VALUE)
+									.addComponent(panel_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 664, Short.MAX_VALUE))
+								.addGap(60)))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(247)
+							.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
 								.addComponent(Save_btn, GroupLayout.PREFERRED_SIZE, 279, GroupLayout.PREFERRED_SIZE)
 								.addComponent(Uavailable_btn, GroupLayout.PREFERRED_SIZE, 279, GroupLayout.PREFERRED_SIZE))
-							.addGap(178)))
-					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
-						.addComponent(panel_5, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 214, Short.MAX_VALUE)
-						.addComponent(selectR_comboBox, Alignment.LEADING, 0, 214, Short.MAX_VALUE))
-					.addGap(156))
+							.addGap(260)))
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(selectR_comboBox, 0, 255, Short.MAX_VALUE)
+							.addGap(45))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(panel_5, GroupLayout.PREFERRED_SIZE, 255, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap())))
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
 						.addGroup(gl_panel.createSequentialGroup()
 							.addGap(33)
 							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-								.addComponent(selectR_comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblNewLabel_2))
-							.addGap(41)
-							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_panel.createSequentialGroup()
-									.addGap(18)
-									.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 142, GroupLayout.PREFERRED_SIZE)
-									.addGap(35)
-									.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, 190, GroupLayout.PREFERRED_SIZE)
-									.addGap(18)
-									.addComponent(Uavailable_btn)
-									.addGap(18)
-									.addComponent(Save_btn, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
-								.addGroup(gl_panel.createSequentialGroup()
-									.addGap(7)
-									.addComponent(panel_5, GroupLayout.PREFERRED_SIZE, 458, GroupLayout.PREFERRED_SIZE))))
+								.addComponent(selectR_comboBox, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNewLabel_2)))
 						.addGroup(gl_panel.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(171, Short.MAX_VALUE))
+							.addGap(24)
+							.addComponent(panel_3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(58)
+							.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 164, GroupLayout.PREFERRED_SIZE)
+							.addGap(60)
+							.addComponent(panel_4, GroupLayout.DEFAULT_SIZE, 210, Short.MAX_VALUE)
+							.addGap(18)
+							.addComponent(Uavailable_btn, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(Save_btn, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(18)
+							.addComponent(panel_5, GroupLayout.DEFAULT_SIZE, 588, Short.MAX_VALUE)))
+					.addGap(63))
 		);
 		
 		JScrollPane Darea_scrollPane = new JScrollPane();
@@ -224,39 +233,65 @@ public class DeliveryView {
 		Darea_Field.setColumns(10);
 		
 		JButton Add_btn = new JButton("Add");
-		Add_btn.setBackground(Color.YELLOW);
+		Add_btn.setBackground(Color.BLACK);
+		Add_btn.setForeground(Color.white);
 		
 		JButton Delete_btn = new JButton("Delete");
-		Delete_btn.setBackground(Color.YELLOW);
+		Delete_btn.setBackground(Color.BLACK);
+		Delete_btn.setForeground(Color.white);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.RED);
 		GroupLayout gl_panel_5 = new GroupLayout(panel_5);
 		gl_panel_5.setHorizontalGroup(
 			gl_panel_5.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_5.createSequentialGroup()
+					.addGap(21)
 					.addGroup(gl_panel_5.createParallelGroup(Alignment.LEADING)
-						.addComponent(Darea_scrollPane, GroupLayout.PREFERRED_SIZE, 212, GroupLayout.PREFERRED_SIZE)
 						.addGroup(gl_panel_5.createSequentialGroup()
-							.addGap(10)
-							.addComponent(Darea_Field, GroupLayout.PREFERRED_SIZE, 192, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap())
-				.addGroup(gl_panel_5.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(Add_btn, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
-					.addComponent(Delete_btn, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
-					.addGap(21))
+							.addComponent(Darea_Field, GroupLayout.PREFERRED_SIZE, 192, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap())
+						.addGroup(gl_panel_5.createSequentialGroup()
+							.addComponent(Add_btn, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
+							.addComponent(Delete_btn, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+							.addGap(24))))
+				.addComponent(Darea_scrollPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 253, Short.MAX_VALUE)
+				.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, 253, Short.MAX_VALUE)
 		);
 		gl_panel_5.setVerticalGroup(
 			gl_panel_5.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_5.createSequentialGroup()
-					.addComponent(Darea_scrollPane, GroupLayout.PREFERRED_SIZE, 342, GroupLayout.PREFERRED_SIZE)
+					.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 96, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(Darea_scrollPane, GroupLayout.PREFERRED_SIZE, 365, GroupLayout.PREFERRED_SIZE)
 					.addGap(18)
 					.addComponent(Darea_Field, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
 					.addGap(18)
-					.addGroup(gl_panel_5.createParallelGroup(Alignment.LEADING)
-						.addComponent(Add_btn, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE)
-						.addComponent(Delete_btn, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addGroup(gl_panel_5.createParallelGroup(Alignment.BASELINE)
+						.addComponent(Add_btn, GroupLayout.DEFAULT_SIZE, 41, Short.MAX_VALUE)
+						.addComponent(Delete_btn, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE))
 					.addContainerGap())
 		);
+		
+		JLabel label = new JLabel("Delivery Area");
+		label.setFont(new Font("Arial", Font.BOLD, 20));
+		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
+		gl_panel_2.setHorizontalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_2.createSequentialGroup()
+					.addGap(57)
+					.addComponent(label, GroupLayout.PREFERRED_SIZE, 125, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(71, Short.MAX_VALUE))
+		);
+		gl_panel_2.setVerticalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_2.createSequentialGroup()
+					.addGap(35)
+					.addComponent(label, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(37, Short.MAX_VALUE))
+		);
+		panel_2.setLayout(gl_panel_2);
 		
 		Darea_table = new JTable();
 		Darea_table.setModel(new DefaultTableModel(
@@ -296,18 +331,22 @@ public class DeliveryView {
 		gl_panel_4.setHorizontalGroup(
 			gl_panel_4.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_4.createSequentialGroup()
-					.addGap(21)
 					.addGroup(gl_panel_4.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblNewLabel_4)
-						.addComponent(lblPassword, GroupLayout.PREFERRED_SIZE, 66, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 147, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblConfirmPassword))
-					.addPreferredGap(ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
-					.addGroup(gl_panel_4.createParallelGroup(Alignment.LEADING)
-						.addComponent(User_Field, GroupLayout.PREFERRED_SIZE, 162, GroupLayout.PREFERRED_SIZE)
-						.addComponent(pass_Field, GroupLayout.PREFERRED_SIZE, 162, GroupLayout.PREFERRED_SIZE)
-						.addComponent(ConfirmPass_Field, GroupLayout.PREFERRED_SIZE, 162, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(44, Short.MAX_VALUE))
+						.addGroup(gl_panel_4.createSequentialGroup()
+							.addGap(65)
+							.addGroup(gl_panel_4.createParallelGroup(Alignment.TRAILING)
+								.addComponent(lblNewLabel_4, Alignment.LEADING)
+								.addComponent(lblConfirmPassword, Alignment.LEADING)
+								.addComponent(lblPassword, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE))
+							.addGap(71)
+							.addGroup(gl_panel_4.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(ConfirmPass_Field)
+								.addComponent(pass_Field)
+								.addComponent(User_Field, GroupLayout.DEFAULT_SIZE, 346, Short.MAX_VALUE)))
+						.addGroup(gl_panel_4.createSequentialGroup()
+							.addGap(21)
+							.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 169, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(70, Short.MAX_VALUE))
 		);
 		gl_panel_4.setVerticalGroup(
 			gl_panel_4.createParallelGroup(Alignment.LEADING)
@@ -317,34 +356,32 @@ public class DeliveryView {
 					.addGap(18)
 					.addGroup(gl_panel_4.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblNewLabel_4)
-						.addComponent(User_Field, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addComponent(User_Field, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE))
+					.addGap(23)
 					.addGroup(gl_panel_4.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblPassword, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE)
-						.addComponent(pass_Field, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addComponent(pass_Field, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
 					.addGroup(gl_panel_4.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblConfirmPassword, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE)
-						.addComponent(ConfirmPass_Field, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(68, Short.MAX_VALUE))
+						.addComponent(ConfirmPass_Field, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(51, Short.MAX_VALUE))
 		);
 		panel_4.setLayout(gl_panel_4);
 		
 		JLabel lblNewLabel = new JLabel("Delivery View");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 22));
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 30));
 		GroupLayout gl_panel_3 = new GroupLayout(panel_3);
 		gl_panel_3.setHorizontalGroup(
 			gl_panel_3.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_3.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblNewLabel)
-					.addContainerGap(81, Short.MAX_VALUE))
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 226, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		gl_panel_3.setVerticalGroup(
-			gl_panel_3.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_panel_3.createSequentialGroup()
-					.addContainerGap(18, Short.MAX_VALUE)
-					.addComponent(lblNewLabel)
+			gl_panel_3.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, gl_panel_3.createSequentialGroup()
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 32, Short.MAX_VALUE)
 					.addContainerGap())
 		);
 		panel_3.setLayout(gl_panel_3);
